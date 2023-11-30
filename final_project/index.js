c085920f36d672d5ca4af31722f6393d54bdf4ae
jellycoolgu <jellycoolgu@gmail.com>
2023-11-30 00:04:51 -0800

    completed task1-task14

--- final_project/index.js
+++ final_project/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+//const axios = require('axios');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
+
+let users = []
+
 
 const app = express();
 
@@ -12,8 +16,22 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+    if(req.session.authorization) {
+        token = req.session.authorization['myAccessToken'];
+        jwt.verify(token, "access",(err,user)=>{
+            if(!err){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "User not authenticated"})
+            }
+        });
+    } else {
+        return res.status(403).json({message: "User not logged in"})
+    }
 });
- 
+  
 const PORT =5000;
 
 app.use("/customer", customer_routes);
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,24 +7,113 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+  // Criteria for a valid username
+  // - Should have a minimum length of 3 characters
+  // - The first character should be a letter or an underscore
+  // - The remaining characters should be letters, numbers, or special characters without space
+
+  const regex = /^[A-Za-z_][^\s]{2,}$/;
+  let isvalid = regex.test(username);
+  return isvalid;
+
+}
+
+const isValidPassword = (password) => {
+  // Check if the password length is more than 7 characters
+  if (password.length < 8) {
+    return false;
+  };
+
+  // Check if the password contains at least one lowercase letter
+  if (!/[a-z]/.test(password)) {
+    return false;
+  };
+
+  // Check if the password contains at least one uppercase letter
+  if (!/[A-Z]/.test(password)) {
+    return false;
+  };
+
+  // Check if the password contains at least one number
+  if (!/[0-9]/.test(password)) {
+    return false;
+  };
+
+  // Check if the password contains at least one special character
+  if (!/[!@#$%^&*()\-_=+{}[\]|;:'",.<>/?`~]/.test(password)) {
+    return false;
+  };
+
+  // All criteria are met, password is valid
+  return true;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+  let validuser = users.filter((user) => {
+    return (user.username === username && user.password === password)
+  });
+  if (validuser.length > 0){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({message: "Error logging in"});
+  }
+
+  if (authenticatedUser(username,password)){
+    let myAccessToken = jwt.sign({data:password},'access',{expiresIn:60*60*12});
+    req.session.authorization = { myAccessToken, username};
+    return res.status(200).json({message: "User successfully logged in"});
+
+  } else{
+    return res.status(208).json({message: "Invalid login. Check username and password"});
+  }
+  
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const isbn = req.params.isbn;
+  const newreview = req.body.review; 
+  let thebook = books[isbn];
+  let reviews = thebook.reviews // for example {"user1":"reviews1", "user2":"reviews2"}
+  let currentUser = req.session.authorization.username;
+  let theReviews = reviews[currentUser];
+
+  if (!(newreview ===theReviews)){
+    reviews[currentUser] = newreview;
+    res.send(` the user ${currentUser} 's reviews updated`);
+  }
+});
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  let thebook = books[isbn];
+  let reviews = thebook.reviews // for example {"user1":"reviews1", "user2":"reviews2"}
+  let currentUser = req.session.authorization.username;
+  let theReviews = reviews[currentUser];
+
+  if (theReviews.length > 0){
+    delete reviews[currentUser];
+    res.send(` the user ${currentUser} 's reviews has been deleted.`);
+  }
+
 });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
+module.exports.isValidPassword = isValidPassword;
+
